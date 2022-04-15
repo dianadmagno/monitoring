e8cf01b3e81486ce853caf30ed26492e1cbb06ab
dianadmagno <dianamagno20@gmail.com>
2022-04-15 21:25:29 +0800

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+        if(Auth::id()) {
+            if (Auth::user()->usertype == '0') {
+                return view('user.home');
+            } else {
+                return view('admin.home');
+            }
+        }
+    }
+
+    public function index()
+    {
+        return view('user.home');
+    }
+}
